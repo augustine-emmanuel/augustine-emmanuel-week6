67621c77b4cdfa1fd01e3ebf75e7cd661b044edc
augustine-emmanuel <emmaustin20@gmail.com>
2025-03-10 04:15:52 +0100

    initial commit

---
+++ retrieve_data.sh
@@ -0,0 +1,44 @@
+#!bin/bash
+
+VERSION="$(yq eval '.version' params.yml)"
+SIZE=$(yq eval ".size.\"${VERSION}\"" params.yml)
+
+URL_LINK="https://jsonplaceholder.typicode.com/photos"
+
+DATA_LOCATION="datahub/data.json"
+temp="mktemp"
+
+curl "${URL_LINK}" | jq '.[:$SIZE]' > $temp
+
+if [[ ! -f $DATA_LOCATION ]]
+  then
+  touch ${DATA_LOCATION}
+fi
+
+if ! diff -q ${DATA_LOCATION} $temp
+  then
+
+    mv $temp $DATA_LOCATION
+  else
+
+    echo "No changes; data has not changed"
+
+    rm $temp
+
+    exit
+fi
+
+
+cat <-EOF
+
+The current data version is: $VERSION
+
+The total dataset size: $(jq '. | length' $DATA_LOCATION)
+
+Sample rows: $(jq '.[:3] $DATA_LOCATION)
+EOF>
+
+
+
+EOF>
+
